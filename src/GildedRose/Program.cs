229ce5eec17b3b706ca8a2cedc1ea7f45a82bd6f
TwoByteBaker <86556388+OneByteBakery@users.noreply.github.com>
2024-12-07 15:44:02 +0000

    GR-4 - Updated Program to accept the number of days to simulate as an argument

--- src/GildedRose/Program.cs
+++ src/GildedRose/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace GildedRoseKata
 {
-    public class Program
+    internal static class Program
     {
-        public static void Main(string[] args)
+        /// <summary>
+        /// Simulates the passage of time over the requested number of days, producing a statement of depreciating quality over time to the console standard output stream.
+        /// </summary>
+        /// <param name="args">Requires exactly one argument which represents a valid <see cref="uint"/> value (e.g. 30)</param>
+        /// <exception cref="ArgumentException">When the supplied args contains an incorrect number of values, or a non-positive integer</exception>
+        internal static void Main([NotNull] string[] args)
         {
-            Console.WriteLine("OMGHAI!");
+            if (args?.Length != 1)
+            {
+                throw new ArgumentException($"Exactly one argument must be supplied. Received {args?.Length ?? 0}");
+            }
 
-            IList<Item> Items = new List<Item>{
+            if (!uint.TryParse(args[0], out var numberOfDays))
+            {
+                throw new ArgumentException("Supplied argument must be a positive integer");
+            }
+            
+            var items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -37,15 +51,16 @@ namespace GildedRoseKata
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
-            var app = new GildedRose(Items);
+            var app = new GildedRose(items);
 
-            for (var i = 0; i < 31; i++)
+            Console.WriteLine("OMGHAI!");
+            for (var i = 0; i <= numberOfDays; i++)
             {
-                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine($"-------- day {i} --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                foreach (var item in items)
                 {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                    Console.WriteLine(item.Name + ", " + item.SellIn + ", " + item.Quality);
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
